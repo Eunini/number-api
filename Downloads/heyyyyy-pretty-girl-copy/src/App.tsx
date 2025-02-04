7216ffb87454454f30893d07e4d4045b5b6697a0
eunice <atandainioluwa371@gmail.com>
2025-02-04 05:39:35 -0800

    initial

---
+++ Downloads/heyyyyy-pretty-girl-copy/src/App.tsx
@@ -0,0 +1,164 @@
+import { useState, useEffect } from "react";
+import { useWindowSize } from "react-use";
+import Confetti from "react-confetti";
+import { motion } from "framer-motion";
+import { LogSnag } from "@logsnag/node";
+
+const logsnag = new LogSnag({
+  token: "LOGSNAG_TOKEN",
+  project: "PROJECT_NAME",
+});
+
+const track = async () => {
+  await logsnag.track({
+    channel: "yes",
+    event: "Valentine's Day",
+    description: "She said yes!",
+    icon: "💖",
+    notify: true,
+  });
+};
+
+function App() {
+  const steps = [
+    {
+      content: "My Dearest Elizabeth,",
+      image: "/character/one.png",
+    },
+    {
+      content: `Knowing that you bring me peace, I hope you find strength to keep pushing forward.
+      `,
+      image: "/character/two.png",
+    },
+    {
+      content: `Since the day we met, you've been etched into my mind- perhaps into my heart. And the night at Heritage park,
+      I knew it for sure- I love this girl.
+      `,
+      image: "/character/three.png",
+    },
+    {
+      content: `You're Caring, Loving and strong your radiant smile makes my thoughts process pause.`,
+      image: "/character/four.png",
+    },
+    {
+      content: `I cherish every moment we spend together. Every time, I long to hold you hands a little longer.`,
+      image: "/character/five.png",
+    },
+    {
+      content: "And so with one last arrow left in Cupid's quiver, I take my shot--",
+      image: "/character/six.png",
+    },
+    {
+      content: "Elizabeth, Will you be my Valentine?",
+      image: "/character/seven.png",
+    },
+  ];
+  const [currentStep, setCurrentStep] = useState(0);
+  const [sheWantsToBeMyValentine, setHeWantsToBeMyValentine] = useState(false);
+  const { width, height } = useWindowSize();
+
+  useEffect(() => {
+    const imagePaths = [
+      ...steps.map((step) => step.image),
+      "/character/yayyyy.png",
+    ];
+
+    imagePaths.forEach((path) => {
+      const img = new Image();
+      img.src = path;
+    });
+  }, []);
+
+  return (
+    <>
+      {sheWantsToBeMyValentine && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Confetti width={width} height={height} />
+          <div className="fixed top-0 left-0 w-full h-full bg-[#A67B5B] flex flex-col items-center justify-center">
+            <motion.h1
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              transition={{ delay: 0.3, type: "spring" }}
+              className="text-white text-4xl font-bold"
+            >
+              Yayyyyyyy!!!!!
+            </motion.h1>
+            <img
+              src="/character/yayyyy.png"
+              alt=""
+              className="w-40 animate-bounce"
+            />
+          </div>
+        </motion.div>
+      )}
+      <div className="bg-[#A67B5B] min-h-screen text-white p-5 flex flex-col items-center justify-center max-w-md mx-auto">
+        <motion.img
+          key={currentStep}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+          src={steps[currentStep].image}
+          alt=""
+          className="w-40"
+        />
+        <motion.div
+          key={currentStep + "-text"}
+          initial={{ opacity: 0, x: -20 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className="font-josefin text-4xl font-bold"
+        >
+          {steps[currentStep].content}
+        </motion.div>
+
+        {currentStep < 6 && (
+          <>
+            <button
+              onClick={() => setCurrentStep(currentStep + 1)}
+              className="bg-white text-[#A67B5B] py-3 text-xl rounded-xl w-full mt-10 font-semibold"
+            >
+              Next
+            </button>
+            {currentStep > 0 && (
+              <button
+                onClick={() => setCurrentStep(currentStep - 1)}
+                className="bg-white text-[#A67B5B] py-3 text-xl rounded-xl w-full mt-2 font-semibold opacity-90"
+              >
+                Back
+              </button>
+            )}
+          </>
+        )}
+        {currentStep === 6 && (
+          <>
+            <button
+              onClick={async () => {
+                setHeWantsToBeMyValentine(true);
+                await track();
+              }}
+              className="bg-white text-[#A67B5B] py-3 text-xl rounded-xl w-full mt-10 font-semibold"
+            >
+              Yes
+            </button>
+
+            <button
+              onClick={async () => {
+                setHeWantsToBeMyValentine(true);
+                await track();
+              }}
+              className="bg-white text-[#A67B5B] py-3 text-xl rounded-xl w-full mt-2 font-semibold"
+            >
+              Yes
+            </button>
+          </>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default App;
