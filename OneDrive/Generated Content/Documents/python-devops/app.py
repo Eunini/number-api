30cda7f1458ea8908760714c3209aed944b046fe
eunice <atandainioluwa371@gmail.com>
2025-02-07 02:12:47 -0800

    Initial commit - Flask Number Classification API

--- OneDrive/Generated Content/Documents/python-devops/app.py
+++ OneDrive/Generated Content/Documents/python-devops/app.py
@@ -1,17 +1,44 @@
-from flask import Flask, request, jsonify 
+from flask import Flask, request, jsonify
 import requests
 
 app = Flask(__name__)
 
+def is_prime(n):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    max_divisor = int(n**0.5) + 1
+    for d in range(3, max_divisor, 2):
+        if n % d == 0:
+            return False
+    return True
+
+def is_perfect(n):
+    sum_divisors = sum(i for i in range(1, n) if n % i == 0)
+    return sum_divisors == n
+
+def is_armstrong(n):
+    return sum(int(digit) ** len(str(n)) for digit in str(n)) == n
+
+def get_fun_fact(val):
+    try:
+        response = requests.get(f'http://numbersapi.com/{val}?json=true')
+        return response.json().get('text', 'No fun fact available.')
+    except:
+        return "Could not fetch fun fact."
+
 @app.route('/api/classify-number', methods=['GET'])
 def classify_number():
     number = request.args.get('number')
-    if not number.isdigit():
-        return jsonify({'number': number, 'error': True}), 400
+    if not number or not number.isdigit():
+        return jsonify({'error': 'Invalid number'}), 400
 
     number = int(number)
     properties = []
-    
+
     if is_prime(number):
         properties.append('prime')
     if is_perfect(number):
@@ -32,31 +59,6 @@ def classify_number():
     }
 
     return jsonify(response), 200
-    def is_prime(n):
-        if n > 1:
-            return False
-        if n == 2:
-            return True
-        if n % 2 == 0:
-            return False
-
-max_divisor = int(n**0.5) + 1
-for d in range(3, max_divisor, 2):
-    if n % d == 0:
-        return False
-    return True
-
-def is_perfect(n):
-    if n % i == 0:
-        sum += i
-    return sum == n
-
-def is_armstrong(n):
-    return sum(int(digit) ** len(str(n)) for digit in str(n) == n)
-
-def get_fun_fact(val):
-    response = requests.get(f'http://numbersapi.com/{val}?json=true')
-    return response.json()['text']
 
 if __name__ == '__main__':
     app.run(debug=True)
